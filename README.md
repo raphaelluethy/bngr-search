81b08649f9fe504100985d527c109a7e0ed2d23c
Raphael Lüthy <raphael.luethy@fhnw.ch>
2025-02-18 00:13:00 +0100

    i am batman

---
+++ README.md
@@ -0,0 +1,12 @@
+# bngr Search
+
+Use bngr to use bangs with your search engine of choice. This Raycast extension allows you to quickly search using bngr.ch.
+
+## Usage
+
+The extension provides two main actions:
+
+-   **Search with Bngr:** Opens the search query directly on bngr.ch using the default search.
+-   **Pipe Search to Default Search:**  Opens the search query on bngr.ch, piping the search to the default search engine specified on the site.  This can be triggered with `Cmd + Return`.
+
+You can also find more information and configure your default search engine on [bngr.ch](https://bngr.ch).
---
+++ src/bngr.tsx
@@ -0,0 +1,56 @@
+import {
+	closeMainWindow,
+	open,
+	Action,
+	ActionPanel,
+	List,
+	getPreferenceValues,
+} from "@raycast/api";
+import { useState } from "react";
+
+interface Preferences {
+	customSearchUrl?: string;
+}
+
+const DEFAULT_SEARCH_URL = "https://bngr.ch?e=k";
+const SEARCH_URL =
+	getPreferenceValues<Preferences>().customSearchUrl || DEFAULT_SEARCH_URL;
+
+const SEARCH_TYPES = {
+	default: (query: string) => `${SEARCH_URL}&q=${encodeURIComponent(query)}`,
+	pipe: (query: string) => `${SEARCH_URL}&q=|${encodeURIComponent(query)}`,
+};
+
+export default function Command() {
+	const [searchText, setSearchText] = useState("");
+
+	return (
+		<List
+			searchText={searchText}
+			searchBarPlaceholder="Enter search text..."
+			onSearchTextChange={setSearchText}
+			throttle={false}
+			actions={
+				<ActionPanel>
+					<Action
+						title="Search with Bngr"
+						onAction={async () => {
+							const url = SEARCH_TYPES.default(searchText);
+							await closeMainWindow();
+							await open(url);
+						}}
+					/>
+					<Action
+						title="Pipe Search to Default Search"
+						shortcut={{ modifiers: ["cmd"], key: "return" }}
+						onAction={async () => {
+							const url = SEARCH_TYPES.pipe(searchText);
+							await closeMainWindow();
+							await open(url);
+						}}
+					/>
+				</ActionPanel>
+			}
+		/>
+	);
+}
